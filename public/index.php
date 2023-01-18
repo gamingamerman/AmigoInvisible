ec02eec99a389a2a6a22335764803076a6e9733b
gamingamerman <gamingamerman@gmail.com>
2023-01-18 19:51:33 +0100

    new chanes

--- public/index.php
+++ public/index.php
@@ -1,12 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Amigo Invisible</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+require_once '../vendor/autoload.php';
+
+// Router system
+$router = new AltoRouter();
+
+// Lista de rutas
+$router -> map('GET', '/cosa', function() {
+    echo 'Hola cosa';
+});
+
+// End of List
+
+$match = $router->match();
+if($match) {
+ $target = $match["target"];
+ if(is_string($target) && strpos($target, "#") !== false) {
+     list($controller, $action) = explode("#", $target);
+     $controller = "Controller\\" . $controller;
+     $controller = new $controller;
+     $controller->$action($match["params"]);
+ } else {
+     if(is_callable($match["target"])) call_user_func_array($match["target"], $match["params"]);
+     else require $match["target"];
+ }
+} else {
+ echo "Ruta no válida";
+ die();
+}
